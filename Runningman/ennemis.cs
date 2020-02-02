1d90b161ad05ecc12dad3a226f10b540f6efd7a2
Johanna Levasseur <johanna.levasseur@epita.fr>
2020-02-02 22:30:46 +0100

    ennemi

--- Runningman/ennemis.cs
+++ Runningman/ennemis.cs
@@ -5,7 +5,13 @@ public class ennemis : KinematicBody2D
 {
     public override void _Ready()
     {
-        
+        Input.is_action_pressed("ui_right");
     }
 
+}
+
+
+private void _on_Timer_timeout()
+{
+    // Replace with function body.
 }
