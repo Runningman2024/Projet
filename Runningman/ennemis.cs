704aada20a967d2eb23b6fbdf6cd8f04aeb0a5d4
Johanna Levasseur <johanna.levasseur@epita.fr>
2020-02-03 13:54:04 +0100

    johanna

--- Runningman/ennemis.cs
+++ Runningman/ennemis.cs
@@ -3,15 +3,56 @@ using System;
 
 public class ennemis : KinematicBody2D
 {
-    public override void _Ready()
+	public int Speed = 400; // How fast the player will move (pixels/sec).
+
+    private Vector2 _screenSize; // Size of the game window.
+}
+
+public override void _Ready()
+{
+    _screenSize = GetViewport().GetSize();
+}
+
+public override void _Process(float delta)
+{
+    var velocity = new Vector2(); // The player's movement vector.
+
+    if (Input.IsActionPressed("ui_right"))
     {
-        Input.is_action_pressed("ui_right");
+        velocity.x += 1;
     }
 
+    if (Input.IsActionPressed("ui_left"))
+    {
+        velocity.x -= 1;
+    }
+
+    if (Input.IsActionPressed("ui_down"))
+    {
+        velocity.y += 1;
+    }
+
+    if (Input.IsActionPressed("ui_up"))
+    {
+        velocity.y -= 1;
+    }
+
+    var animatedSprite = GetNode<AnimatedSprite>("AnimatedSprite");
+
+    if (velocity.Length() > 0)
+    {
+        velocity = velocity.Normalized() * Speed;
+        animatedSprite.Play();
+    }
+    else
+    {
+        animatedSprite.Stop();
+    }
+	
+	Position += velocity * delta;
+	Position = new Vector2(
+    x: Mathf.Clamp(Position.x, 0, _screenSize.x),
+    y: Mathf.Clamp(Position.y, 0, _screenSize.y)
+);
 }
 
-
-private void _on_Timer_timeout()
-{
-    // Replace with function body.
-}
