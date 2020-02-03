33cbc04aaf0dd730df4b6db7fd56a92c11ceb4e7
Maya Leonardon <maya.leonardon@epita.fr>
2020-02-03 18:04:35 +0100

    maya monde 1

--- Runningman/ennemis.cs
+++ Runningman/ennemis.cs
@@ -3,15 +3,15 @@ using System;
 
 public class ennemis : KinematicBody2D
 {
-    public override void _Ready()
-    {
-        Input.is_action_pressed("ui_right");
-    }
+	public override void _Ready()
+	{
+		Input.is_action_pressed("ui_right");
+	}
 
 }
 
 
 private void _on_Timer_timeout()
 {
-    // Replace with function body.
+	// Replace with function body.
 }
